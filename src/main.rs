fc4942f524ea14805e33521fa3ec9a90508fe3d8
Jacob Salzberg <salzber@amazon.com>
2024-05-30 18:15:06 -0400

    Uploading an example of a crate that I think should compile

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate kani;
+use kani::*;
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Zfunction-contracts
+
+// Test that a modifies clause works when a (function call)
+// expression is provided
+fn infinite_looper() -> usize {
+    loop {};
+    1
+}
+
+#[kani::requires(arr[idx] == 1)]
+#[kani::modifies(&arr[infinite_looper()])]
+#[kani::ensures(arr[idx] == 2)]
+fn modify(idx: usize, arr: &mut [i64]) {
+    arr[idx] += 1;
+}
+
+#[kani::proof_for_contract(modify)]
+fn main() {
+    let mut arr: [i64; 4] = [1, 2, 3, 4];
+    modify(1, &mut arr);
+}
